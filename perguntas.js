cfbe59aefc2b035d8d38ec5dbfa45e0239b59562
felipexx212 <145133471+felipexx212@users.noreply.github.com>
2024-10-16 14:01:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Perguntas',
+    'Quem sou eu?',
+    'Sou o Felipe Matheus de Oliveira'
+)
+
+criaCartao(
+    'Perguntas',
+    'Quantos anos eu tenho?',
+    'Tenho 16 anos'
+)
+
+criaCartao(
+    'Perguntas',
+    'Que dia é meu aniversário?',
+    'Eu nasci no dia 30 de agosto em 2008'
+)
+
+criaCartao(
+    'Perguntas',
+    'Qual é a minha cor favorita?',
+    'Vermelho'
+)
+criaCartao(
+    'Perguntas',
+    'Pra que time eu torço?',
+    'Flamengo'
+)
